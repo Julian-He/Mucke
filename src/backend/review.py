78d30f96c72d0a3b09eab33939fb7a8ef034cfe4
julian <heruthjulian@gmail.com>
2024-03-29 04:51:17 +0100

    made review a dataclass

--- src/backend/review.py
+++ src/backend/review.py
@@ -15,11 +15,9 @@ class Rating:
     release: Release
     ratings: Dict[User, int]
 
+@dataclass
 class Review:
-    def __init__(self, host: User, participants: List[User] = []) -> None:
-        self.__host = host
-        self.__participants = participants
-        self.__music_suggestions: List[MusicSuggestion] = []
-        self.__ratings: List[Rating] = []
-
-        
+    host: User
+    participants: List[User]
+    music_suggestions: List[MusicSuggestion]
+    ratings: List[Rating]
