b92ccf5bcb7df3d0308375ec2c0e6c4401bbcc44
julian <heruthjulian@gmail.com>
2024-04-03 22:06:39 +0200

    restructured data and put it into one file

---
+++ src/backend/data.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from abc import ABC
+from typing import Dict, List
+from discogs_client import Release
+
+
+@dataclass
+class Review(ABC):
+    pass
+
+
+@dataclass
+class User:
+    name: str
+    reviews: List[Review]
+
+
+@dataclass
+class Rating:
+    score: int
+    notes: str
+
+@dataclass
+class ReleaseReview(Review):
+    release: Release
+    suggested_by: User
+    scores: Dict[User, Rating]
+    
+@dataclass
+class ReviewProcess:
+    host: User
+    participants: List[User]
+    reviews: List[ReleaseReview]
--- src/backend/data/user.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import List
-
-from review import Rating
-
-@dataclass
-class User:
-    name: str
-    ratings: List[Rating]
