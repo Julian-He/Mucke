ba2cb2bd981e0f16d9faa68fe949f2f3862bf381
julian <heruthjulian@gmail.com>
2024-04-04 10:53:43 +0200

    create_user usecase unnecessary due to data restructuring

--- src/backend/usecases/create_user.py
+++
@@ -1,8 +0,0 @@
-from backend.data.appdata import AppData
-from backend.data.user import User
-
-
-def create_user(appdata: AppData, name: str) -> AppData:
-    user: User = User(name, [])
-    appdata.users.append(user)
-    return appdata
