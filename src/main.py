7c4062eaff557a24e559094d46bbdf48d754c15f
julian <heruthjulian@gmail.com>
2024-04-04 14:16:36 +0200

    extracted login into own file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from typing import List
 from fastapi import FastAPI, HTTPException, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -9,6 +9,12 @@ from backend.data import User
 users: List[User] = []
 api = FastAPI()
 templates: Jinja2Templates = Jinja2Templates("frontend/templates")
+
+
+@api.get("/login", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="login.html")
+
 
 @api.get("/library", response_class=HTMLResponse)
 async def library(request: Request):
@@ -22,14 +28,18 @@ async def library(request: Request):
     """
     return HTMLResponse(html_content)
 
-@api.post("/login", response_class=HTMLResponse)
+
+@api.post("/homepage", response_class=HTMLResponse)
 async def login(request: Request, name: str = Form(...)):
-    return templates.TemplateResponse(
-        request=request,
-        name="homepage.html",
-        context={"header": name}
-    )
-        
+
+    if name not in [user.name for user in users]:
+        return HTTPException(400, "bad")
+
+    else:
+        return templates.TemplateResponse(
+            request=request, name="homepage.html", context={"header": name}
+        )
+
 
 api.mount("/frontend", api)
 api.mount("/", StaticFiles(directory="frontend", html=True))
