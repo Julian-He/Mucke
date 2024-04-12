4eaa1f5cb35d5fa8813a0afe96bf3e7355a7dcfd
julian <heruthjulian@gmail.com>
2024-04-12 12:02:44 +0200

    .

--- src/backend/usecases/review.py
+++ src/backend/usecases/review.py
@@ -3,9 +3,10 @@ from discogs_client import Release
 
 from data import User, ReleaseReview, ReviewProcess, AppData
 
-def start_review(all_users: List[User], host: User, hosts_pick: Release) -> AppData:
-    AppData.review_processes.append(
-        ReviewProcess(host, [], ReleaseReview(
-            hosts_pick, host, [,])
-        ))
-        )    
+
+def start_review(appdata: AppData, host: User, hosts_pick: Release) -> AppData:
+    appdata.review_processes.append(
+        ReviewProcess(host, [], [ReleaseReview(hosts_pick, host, {})])
+    )
+
+    return appdata
